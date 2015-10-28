29be09bae112ca6343b03ddd307ef9769ea1d0e0
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2015-10-27 23:05:34 -0400

    edit added

--- app.js
+++ app.js
@@ -11,36 +11,55 @@ var toDo ={
 
   },
   initStyling : function(){
-    toDo.loadToDos(toDoList);
+    toDo.loadToDos(toDo.toDoList);
     toDo.setNumLeft();
 
   },
   initEvents : function(){
     //create new todo
-    $('form').on('submit', function(event){
-    event.preventDefault();
-    var newToDo ={
-      item: $("input[name='newtodo']").val(),
-      status: "Active",
-    };
-    toDoList.push(newToDo);
-    var toDoId = toDoList.indexOf(newToDo);
-    newToDo.id = toDoId;
-    toDo.loadTemplate($('.todo-section'), newToDo, $('#articleTmpl').html());
-    $('input').val('');
+    $('#newForm').on('submit', function(event){
+      event.preventDefault();
+      var newToDo ={
+        item: $("input[name='newtodo']").val(),
+        status: "Active",
+      };
+      toDo.toDoList.push(newToDo);
+      var toDoId = toDo.toDoList.indexOf(newToDo);
+      newToDo.id = toDoId;
+      toDo.loadTemplate($('.todo-section'), newToDo, $('#toDoTmpl').html());
+      $("input[name='newtodo']").val('');
+      toDo.setNumLeft();
     });
+    // edit a to do
+    $('.active-list').on('submit', '.edit-form', function(event){
+      event.preventDefault();
+      var index = $(this).closest('article').data('id');
+      var editResult = $(this).children('input').val();
+      console.log(editResult);
+      console.log(index);
+      toDo.toDoList[index].item = editResult;
+      $(this).toggleClass('invisible');
+      $(this).siblings('section').toggleClass('invisible');
+      $('.todo-section').html("");
+      toDo.loadToDos(toDo.toDoList);
+    });
+    // filter for All
     $('#All').on('click', function(event){
       event.preventDefault();
       toDo.filterVisible("All");
       $(this).addClass('active_filter');
       $(this).siblings('li').removeClass('active_filter');
     });
+
+    // filter for Active only
     $('#Active').on('click', function(event){
       event.preventDefault();
       toDo.filterVisible("Active");
       $(this).addClass('active_filter');
       $(this).siblings('li').removeClass('active_filter');
     });
+
+    // filter for complete
     $('#Complete').on('click', function(event){
       event.preventDefault();
       toDo.filterVisible("Completed");
@@ -53,25 +72,33 @@ var toDo ={
       $(this).siblings('section').toggleClass('strikethrough');
       var index = $(this).closest('article').data('id');
       console.log(index);
-      if(toDoList[index].status === "Active"){
-        toDoList[index].status = "Completed";
+      if(toDo.toDoList[index].status === "Active"){
+        toDo.toDoList[index].status = "Completed";
         $(this).html('&#10003;');
       }
       else{
-        toDoList[index].status = "Active";
+        toDo.toDoList[index].status = "Active";
         $(this).text("");
       }
       toDo.setNumLeft();
     });
+    // delete a to do
     $('.active-list').on('click','.delete', function(event){
       event.preventDefault();
       var toDoId = $(this).closest('article').data('id');
       toDo.deleteToDo(toDoId, $(this));
       toDo.setNumLeft();
     });
+
+    // clear all completed
     $('.active-list').on('click','#clear',function(event){
       event.preventDefault();
       toDo.clearComplete();
+    });
+    // toggle edit on
+    $('.active-list').on('dblclick', 'article section', function(event){
+      $(this).toggleClass('invisible');
+      $(this).siblings('form').toggleClass('invisible');
     });
   },
   loadTemplate : function($el, data, tmpl){
@@ -83,10 +110,10 @@ var toDo ={
     _.each(arr, function(currVal, idx, arr){
       currVal.id = idx;
       if(currVal.status ==="Active"){
-      toDo.loadTemplate($('.todo-section'), currVal, $('#articleTmpl').html());
+      toDo.loadTemplate($('.todo-section'), currVal, $('#toDoTmpl').html());
     }
     else{
-      toDo.loadTemplate($('.todo-section'), currVal, $('#articleTmpl2').html());
+      toDo.loadTemplate($('.todo-section'), currVal, $('#completeToDoTmpl').html());
     }
     });
   },
@@ -94,31 +121,34 @@ var toDo ={
     $('.todo-section').html("");
     var filtered = [];
     if(status ==="All"){
-      filtered = toDoList;
+      filtered = toDo.toDoList;
     }
     else{
-    filtered = toDoList.filter(function(item){
+    filtered = toDo.toDoList.filter(function(item){
       return item.status === status;
     });
   }
     toDo.loadToDos(filtered);
   },
   deleteToDo : function(idx, $el){
-    toDoList.splice(idx,1);
+    toDo.toDoList.splice(idx,1);
     $('.todo-section').html('');
-    toDo.loadToDos(toDoList);
+    toDo.loadToDos(toDo.toDoList);
   },
   clearComplete : function(){
     $('.todo-section').html("");
     var filtered =[];
-    toDoList = toDoList.filter(function(item){
+    toDo.toDoList = toDo.toDoList.filter(function(item){
       return item.status === "Active";
     });
-    toDo.loadToDos(toDoList);
+    //returns to all view to prevent rare instance of completed view showing an active
+    $('#All').addClass('active_filter');
+    $('#All').siblings('li').removeClass('active_filter');
+    toDo.loadToDos(toDo.toDoList);
   },
   setNumLeft: function(){
     $('#num-left').html("");
-    var numLeft = toDoList.filter(function(item){
+    var numLeft = toDo.toDoList.filter(function(item){
       return item.status === "Active";
     }).length;
     if(numLeft === 1){
@@ -127,6 +157,9 @@ var toDo ={
     else{
       $('#num-left').html(numLeft + " items left");
     }
-  }
+  },
+  toDoList : [],
 
 };
+
+//localStorage.setItem('toDo', JSON.stringify(toDo));
--- data.js
+++ data.js
@@ -1,14 +1,6 @@
+//page used for debugging purposes
+
+/*
 var toDoList = [
-  {
-    item: "Clean Garage",
-    status: "Completed"
-  },
-  {
-    item: "Pick up dry-cleaning",
-    status: "Active"
-  },
-  {
-    item: "mow lawn",
-    status: "Active"
-  },
 ];
+*/
